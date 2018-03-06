f68c708607efa6e3dabcf99528244ce2551bce46
Jaagup Kippar <jaagup@tlu.ee>
2018-03-06 09:30:09 +0200

    boot1

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -16,7 +16,13 @@ public class Rakendus {
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
- 
+    
+    @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2){
+        return arv1*arv2;
+    }
+   //http://greeny.cs.tlu.ee:40376/korrutus?arv1=4&arv2=7
+   
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
